aaf6be58f1e88ad5f9439a18e6d360b6170116b7
Omer Yassir <omerym10@gmail.com>
2024-10-27 01:59:31 +0300

    add empty to Priority Queue

--- heap/src/PriorityQueue.cpp
+++ heap/src/PriorityQueue.cpp
@@ -20,11 +20,18 @@ class PriorityQueue
 	};
 	Heap<QueueElement> heap;
 public:
+	bool empty() {
+		return heap.empty();
+	}
 	void insert(T element, int priority) {
 		QueueElement queueElement{ element,priority };
 		heap.push(queueElement);
 	}
 	T extract() {
+		if (empty())
+		{
+			throw std::runtime_error("PriorityQueue is empty, cannot extract element.");
+		}
 		QueueElement queueElement = heap.pop();
 		return queueElement.element;
 	}

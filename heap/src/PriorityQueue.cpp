d21b9d4cb995cba2738cbb506aef795e749de962
Omer Yassir <omerym10@gmail.com>
2024-10-26 23:18:27 +0300

    priority queue

---
+++ heap/src/PriorityQueue.cpp
@@ -0,0 +1,29 @@
+template <typename T>
+class PriorityQueue
+{
+	struct QueueElement {
+		T element;
+		int priority;
+		bool operator<(const QueueElement& other) const {
+			return priority < other.priority;
+		}
+		bool operator<=(const QueueElement& other) const {
+			return priority <= other.priority;
+		}
+		bool operator>(const QueueElement& other) const {
+			return priority > other.priority;
+		}
+		bool operator=>(const QueueElement& other) const {
+			return priority >= other.priority;
+		}
+	};
+	Heap<QueueElement> heap;
+public:
+	void insert(T element, int priority) {
+		QueueElement queueElement{ element,priority };
+		heap.push(queueElement);
+	}
+	T extract() {
+		QueueElement queueElement = heap.pop();
+		return queueElement.element;
+};
